5c35a8c57d44b7658a1b0e645624a0b9dc3c81cb
erik radio <radio.erik@gmail.com>
2017-03-27 14:29:30 -0700

    Update accession2aspace.py

--- accession2aspace.py
+++ accession2aspace.py
@@ -5,7 +5,7 @@ from re import match
 #this script creates accessions from a csv
 
 aspace_url = "http://archivesspacedev.library.arizona.edu:8089"
-username= "radio"
+username= ""
 password = ""
 
 
@@ -13,7 +13,7 @@ auth = requests.post(aspace_url+"/users/"+username+"/login?password="+password).
 session = auth["session"]
 headers = {"X-ArchivesSpace-Session":session}
 
-# print(headers)
+print(headers)
 
 with open("final_accession.csv","rU") as csvFile:
 	reader=csv.DictReader(csvFile)
@@ -34,60 +34,69 @@ with open("final_accession.csv","rU") as csvFile:
 		Notes = row['Notes']
 		AccType = row['Acquisition_Type']
 		Provenance = row['Provenance']
-		# print(DonorLname)
-		# test=requests.get(aspace_url+"/repositories/2/accessions/1",headers=headers).json()
-		# print(test)
-		# make json records for each accession
-		# if len(Provenance) > 0 and len(AccType) > 0:
-		# 	acc_records={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,"extents":[{"jsonmodel_type":"extent","extent_type":"linear_feet","number":Size}],"content_description":Abstract,"resource_type":"collection","acquisition_type":AccType,"general_note":Notes,"provenance":Provenance}
-		# 	acc_as_json = json.dumps(acc_records)
-		# 	print("#########################")
-		# 	print("This is the acc record we're POSTing: \n{}".format(acc_as_json))
-		# 	acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=acc_as_json)
-		# 	print("#########################")
-		# 	print("This is the (raw) response the web server sent:\n{}".format(acc_post.text))
-		# 	print("#########################")
-		# 	print("Now we're trying to make it into JSON")
-		# 	acc_post_response_as_json = acc_post.json()
-		# 	print("We successfully interpretted the webserver response as JSON: \n{}".format(acc_post_response_as_json))
 
 
 
 
 
-		if len(Provenance) > 0 and len(AccType) > 0:
+
+
+
+		if len(Provenance) > 0 and len(AccType) > 0 and len(Size) > 0:
 			acc_records={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
 			"extents":[{"jsonmodel_type":"extent","extent_type":"linear_feet","portion":"whole","number":Size}],
 			"resource_type":"collection","acquisition_type":AccType,"general_note":Notes,"content_description":Abstract,
 			"provenance":Provenance}
 			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_records)).json()
-			print(acc_post.status_code)
-
-
 			print(acc_post)
-		if len(Provenance) > 0 and len(AccType)==0:
+
+		if len(Provenance) > 0 and len(AccType)==0 and len(Size) >  0:
 			acc_records={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
 			"extents":[{"jsonmodel_type":"extent","extent_type":"linear_feet","portion":"whole","number":Size}],
 			"resource_type":"collection","acquisition_type":"gift","general_note":Notes,"content_description":Abstract,
 			"provenance":Provenance}
 			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_records)).json()
-
-
 			print(acc_post)
 
 
 
 
-		if len(Provenance) == 0 and len(AccType) > 0:
+		if len(Provenance) == 0 and len(AccType) > 0 and len(Size) > 0:
 			acc_data={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
 			"extents":[{"jsonmodel_type":"extent","portion":"whole","extent_type":"cubic_feet","number":Size,}],
 			"content_description":Abstract,"resource_type":"collection","acquisition_type":AccType,"general_note":Notes}
 			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_data)).json()
 
 			print(acc_post)
-		if len(Provenance) == 0 and len(AccType) == 0:
+		if len(Provenance) == 0 and len(AccType) == 0 and len(Size) > 0:
 			acc_data={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
 			"extents":[{"jsonmodel_type":"extent","portion":"whole","extent_type":"cubic_feet","number":Size,}],
-			"content_description":Abstract,"resource_type":"collection","acquisition_type":"gift","general_note":Notes}
+			"content_description":Abstract,"resource_type":"collection","general_note":Notes}
 			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_data)).json()
+			print(acc_post)
+
+		if len(Provenance) > 0 and len(AccType) > 0 and len(Size) == 0:
+			acc_records={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
+			"resource_type":"collection","acquisition_type":AccType,"general_note":Notes,"content_description":Abstract,
+			"provenance":Provenance}
+			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_records)).json()
+			print(acc_post)
+
+		if len(Provenance) == 0 and len(AccType) > 0 and len(Size) == 0:
+			acc_records={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
+			"resource_type":"collection","acquisition_type":AccType,"general_note":Notes,"content_description":Abstract}
+			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_records)).json()
+			print(acc_post)
+
+		if len(Provenance) > 0 and len(AccType) == 0 and len(Size) == 0:
+			acc_records={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
+			"resource_type":"collection","general_note":Notes,"content_description":Abstract,
+			"provenance":Provenance}
+			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_records)).json()
+			print(acc_post)
+
+		if len(Provenance) == 0 and len(AccType) == 0 and len(Size) > 0:
+			acc_records={"title":title,"id_0":AccNo,"jsonmodel_type":"accession","accession_date":AccDate,
+			"resource_type":"collection","general_note":Notes,"content_description":Abstract}
+			acc_post=requests.post(aspace_url+"/repositories/2/accessions",headers=headers,data=json.dumps(acc_records)).json()
 			print(acc_post)
