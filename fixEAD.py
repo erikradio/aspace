4c1d2e01a41f1aeb5bcae7ca5a667b06d9c86e01
erik radio <radio.erik@gmail.com>
2017-05-23 11:29:25 -0700

    Update fixEAD.py

--- fixEAD.py
+++ fixEAD.py
@@ -3,33 +3,44 @@ import xml.etree.ElementTree as ET
 import copy
 
 
-def removeColons():
+def removeColons(root):
     for el in root.iter('*'):
         fack=el.attrib
         for x in fack:
-            attrText = fack.get(x)
-            if ':' in attrText:
-                m=attrText.strip(':')
-                print(m)
+            attrText = fack[x]
+            if attrText.endswith(":"):
+                fack[x] = attrText.replace(':','')
+                # print(fack[x])
 
-def updateValues():
+    return root
+
+def updateHeader(root):
     header=root.find('eadheader')
     header.set('findaidstatus','complete')
-
-def getRecord():
-    with open(infile_path, mode='rU') as infile:
-        tree=ET.parse(infile_path)
-        root=tree.getroot()
     
+    return root
 
-def main(tree):
+
+
+
+
+
+def main():
     infile_path = sys.argv[1]
     outfile_path = 'rev_'+sys.argv[1]
 
     tree=ET.parse(infile_path)
+    root=tree.getroot()
+
+    updateHeader(root)
+
+    removeColons(root)
+
+    # print(tree)
     tree.write(outfile_path)
 
 # make this a safe-ish cli script
 if __name__ == '__main__':
+    # print(tree)
 
     main()
